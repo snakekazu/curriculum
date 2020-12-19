00e50683595ed32246dd2029c9176e7936508506
snakekazu <75324232+snakekazu@users.noreply.github.com>
2020-12-19 09:25:10 +0900

    Create 2-11.php

---
+++ 2-11/2-11.php
@@ -0,0 +1,20 @@
+<form action="result.php" method="get">
+  名前：<input type="text" name="my_name" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+<!-- 練習 -->
+<br>
+<!-- ここからした課題 -->
+<form action ="result.php" method="get">
+名前:<input type ="text" name="my_name" />
+<br>
+メールアドレス:<input type ="text" name="e_mail" />
+<br>
+パスワード:<input type ="password" name="password" />
+<br>
+<input type ="submit" value="送信" />
+</form>
+
