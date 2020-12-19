91660b1e3f57dfa6083b1c4c693efcd2c3113791
snakekazu <75324232+snakekazu@users.noreply.github.com>
2020-12-19 18:13:44 +0900

    Create result.php

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$myname = $_POST["myname"];
+$product=$_POST["product"];
+$number=$_POST["number"]
+?>
+<p>お名前:<?php echo $myname; ?></p>
+<p>ご希望景品<?php echo $product; ?></p>
+<p>個数:<?php echo $number; ?></p>
