f2bb0aaeddbbc15dd9928bc5e3183924704aa319
snakekazu <75324232+snakekazu@users.noreply.github.com>
2021-01-03 09:44:31 +0900

    Create form.php

---
+++ 2-18/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><2章チェックテスト</title>
+  <link rel ="stylesheet" type ="text/css" href ="style.css">
+</head>
+<body>
+<div class ="main">
+<h1>2章チェックテスト</h1>
+</div>
+<div class ="contact-form">
+<form method = "post" action ="question.php" >
+<div class ="form-item"><div>
+<input type ="text" name ="name" placeholder="名前を入力してください ">
+<input type ="submit" value ="テスト開始">
+</form>
+</div>
+</div>
+<div class ="footer"></div>
+
+  
+</body>
+</html>
