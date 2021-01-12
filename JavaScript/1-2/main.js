51ba98d52a29022254533ea1938eb9d4a5d3f2ee
snakekazu <75324232+snakekazu@users.noreply.github.com>
2021-01-12 10:39:40 +0900

    Create main.js

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,24 @@
+let name ="hoge";
+console.log(name);
+let ha="hay";
+console.log(ha);
+let animal ="panda";
+console. log(animal)
+
+name ="fuga";
+console.log(name);
+
+name ="池田";
+console.log(name+"君");
+
+let number =10;
+console.log(number *3);
+console.log(`${name}くんは${number}月生まれです`);
+
+// ここから課題
+
+let price =100;
+console.log(price *2);
+
+ price =500
+console.log(price *2);
