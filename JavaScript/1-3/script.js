abd1856a92ee2f800e098ee385f24efbee3bfde7
snakekazu <75324232+snakekazu@users.noreply.github.com>
2021-01-15 10:28:26 +0900

    Create script.js

---
+++ JavaScript/1-3/script.js
@@ -0,0 +1,26 @@
+
+// 問題１
+let a =9;
+let b =12;
+if(a !==10){
+  console.log("変数aは10ではない");
+}
+if(b>=10 && b<20 && b%2===0){
+console.log("・変数bは10以上20未満、かつ偶数である");
+}
+
+// 問題２
+let x =10;
+if(x>=10&&x<=20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+// 問題３
+let y=0;
+if(y%2===0){
+  console.log("偶数です");
+}else{
+  console.log("奇数です");
+}
